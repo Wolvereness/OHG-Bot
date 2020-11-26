865990710a1752c98d1e1203c8a6f52b616fd291
Wesley Wolfe <wesley.d.wolfe+git@gmail.com>
2020-11-26 07:03:14 -0600

    Set up register_role command

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,7 @@
+use std::{
+    result::Result,
+    fmt::Write as _,
+};
 use serenity::{
     async_trait,
     prelude::*,
@@ -11,20 +15,23 @@ use serenity::{
         },
     },
 };
-use futures::{
-    join,
+use futures::join;
+use wither::{
+    Model,
+    mongodb::Database
 };
+use tokio::sync::RwLockReadGuard;
+
 use crate::{
     models::{
         DatabaseHandle,
         RoleAssociation,
     },
-    util::Mentionable,
+    util::{
+        get_role_associations,
+        Mentionable,
+    },
 };
-use std::fmt::Write as _;
-use wither::mongodb::Database;
-use crate::util::get_role_associations;
-use std::result::Result;
 
 #[group]
 #[commands(ping, join, register_role, parrot, dump_associations)]
@@ -223,12 +230,156 @@ async fn leave(ctx: &Context, _msg: &Message, _args: Args) -> CommandResult {
 #[command]
 #[only_in("guild")]
 #[required_permissions("ADMINISTRATOR")]
-async fn register_role(ctx: &Context, _msg: &Message, _args: Args) -> CommandResult {
-    let _db = ctx.data
-        .read()
-        .await
+async fn register_role(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let guild = msg.guild_id.ok_or("No guild present")?;
+    async fn bad_message(ctx: &Context, msg: &Message) -> CommandResult {
+        const CONTENT: &'static str = "One or two parameters.\nOne may be a reference to the channel.\nOne must be either a reference to the group, or the group ID.";
+        msg.reply(ctx, CONTENT).await?;
+        return Ok(());
+    }
+    async fn unamused(ctx: &Context, msg: &Message) -> CommandResult {
+        msg.reply(ctx, ":unamused:").await?;
+        return Ok(());
+    }
+
+    let mut channel: Option<ChannelId> = None;
+    let mut role: Option<RoleId> = None;
+    while !args.is_empty() {
+        match (args.parse(), args.parse()) {
+            (Ok(parsed), Err(_))
+                if channel.is_none() =>
+                    channel = Some(parsed),
+            (_, Ok(parsed))
+                if role.is_none() =>
+                    role = Some(parsed),
+            _ => return bad_message(ctx, msg).await,
+        }
+        args.advance();
+    }
+    if role.is_none() {
+        return bad_message(ctx, msg).await;
+    }
+
+    let db = async {
+        // This is split into a function for ? usage
+        // But, even more-so, it's a two-await that can be done concurrently to the single-awaits
+        async fn get_db_and_associations(ctx: &Context, guild: GuildId, channel: ChannelId)
+            -> CommandResult<(RwLockReadGuard<'_, TypeMap>, Vec<RoleAssociation>)>
+        {
+            let db = ctx.data.read().await;
+            let associations = get_role_associations(
+                db
+                    .get::<DatabaseHandle>()
+                    .ok_or("Database not present")?,
+                channel,
+                guild,
+            ).await?;
+            Ok((db, associations))
+        }
+        get_db_and_associations(ctx, guild, channel.unwrap_or(ChannelId(!0))).await
+    };
+    let channel = async {
+        if let Some(channel) = channel {
+            if let Some(channel) = ctx.cache.guild_channel(channel).await {
+                if channel.guild_id == guild {
+                    Ok(Some(channel.id))
+                } else { Err(()) }
+            } else { Err(()) }
+        } else { Ok(None) }
+    };
+    let role = async {
+        if let Some(role) = role {
+            if let Some(role) = ctx.cache.role(guild, role).await {
+                Ok(Some(role.id))
+            } else { Err(()) }
+        } else { Ok(None) }
+    };
+    let (db, channel, role): (_, Result<Option<ChannelId>, ()>, Result<Option<RoleId>, ()>) = join!(db, channel, role);
+    let (db, associations): (_, Vec<RoleAssociation>) = db?;
+    let db: &Database = db
         .get::<DatabaseHandle>()
         .ok_or("Database not present")?;
+    let (channel, role) = match (channel, role) {
+        (_, Ok(None)) => return bad_message(ctx, msg).await,
+        (Ok(channel), Ok(Some(role))) => (channel, role),
+        _ => return unamused(ctx, msg).await, // Cross-guild poisoning
+    };
+
+    if let Some(channel) = channel {
+        for mut association in associations {
+            if association.channel.is_some() {
+                let old = association.role;
+                association.role = role;
+                association.save(db, None).await?;
+
+                msg.channel_id.send_message(ctx, |message| message.embed(|e| e
+                    .title("Role Association:")
+                    .description(format_args!(
+                        "{} updated to {} from {}",
+                        Mentionable::from(channel),
+                        Mentionable::from(role),
+                        Mentionable::from(old),
+                    ))
+                )).await?;
+                return Ok(());
+            }
+        }
+
+        // None exist; make a new one!
+        RoleAssociation {
+            id: None,
+            channel: Some(channel),
+            server: None,
+            role
+        }
+            .save(db, None)
+            .await?;
+
+        msg.channel_id.send_message(ctx, |message| message.embed(|e| e
+            .title("Role Association:")
+            .description(format_args!(
+                "{} is now associated to {}",
+                Mentionable::from(channel),
+                Mentionable::from(role),
+            ))
+        )).await?;
+    } else {
+        for mut association in associations {
+            if association.server.is_some() {
+                let old = association.role;
+                association.role = role;
+                association.save(db, None).await?;
+
+                msg.channel_id.send_message(ctx, |message| message.embed(|e| e
+                    .title("Role Association:")
+                    .description(format_args!(
+                        "Server's generic role updated to {} from {}",
+                        Mentionable::from(role),
+                        Mentionable::from(old),
+                    ))
+                )).await?;
+                return Ok(());
+            }
+        }
+
+        // None exist; make a new one!
+        RoleAssociation {
+            id: None,
+            channel: None,
+            server: Some(guild),
+            role
+        }
+            .save(db, None)
+            .await?;
+
+        msg.channel_id.send_message(ctx, |message| message.embed(|e| e
+            .title("Role Association:")
+            .description(format_args!(
+                "Server's generic role is now {}",
+                Mentionable::from(role),
+            ))
+        )).await?;
+    }
 
     Ok(())
 }
