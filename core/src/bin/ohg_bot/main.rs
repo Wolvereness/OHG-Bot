32f36aa3a874e9f9b47c35788676c7651031fc6c
Wesley Wolfe <wesley.d.wolfe+git@gmail.com>
2020-12-29 00:37:10 -0600

    Add RPG hook into executable.

--- core/src/bin/ohg_bot/main.rs
+++ core/src/bin/ohg_bot/main.rs
@@ -16,6 +16,11 @@ async fn main() {
                 dump::main().await;
                 return;
             },
+            #[cfg(feature = "rpg")]
+            "--rpg" => {
+                ohg_bot_rpg::main().await;
+                return;
+            },
             _ => {},
         }
     }
